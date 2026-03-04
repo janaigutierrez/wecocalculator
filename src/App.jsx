9bf768b16346400bf027deebd8784ff27d53187b
Janai <janaigs97@gmail.com>
2026-03-04 16:29:34 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-neutral-950 text-neutral-100">
+      <h1 className="text-2xl font-bold p-8">WECO Calculator</h1>
+    </div>
+  )
+}
+
+export default App
